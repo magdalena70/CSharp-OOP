9004df85cc9793b493f39a2735e3764c84d6a23c
Magdalena Petrova <magik70@abv.bg>
2015-01-23 15:08:12 +0200

    03_CalcDistancePoint

---
+++ C_Sharp-OOP/03_CalcDistancePoint/03_CalcDistancePoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Point
+    {
+        //fields
+        private int x_Coord;
+        private int y_Coord;
+
+        //constructor
+        public Point(int x_Coord, int y_Coord)
+        {
+            this.x_Coord = x_Coord;
+            this.y_Coord = y_Coord;
+        }
+
+        //properties
+        public int X_Coord
+        {
+            get
+            {
+                return this.x_Coord;
+            }
+            set
+            {
+                this.x_Coord = value;
+            }
+        }
+
+        public int Y_Coord
+        {
+            get
+            {
+                return this.y_Coord;
+            }
+            set
+            {
+                this.y_Coord = value;
+            }
+        }
+
+        //create method
+        public double CalcDistance(Point p)
+        {
+            return Math.Sqrt((p.x_Coord - this.x_Coord) * (p.x_Coord - this.x_Coord) +
+                             (p.y_Coord - this.y_Coord) * (p.y_Coord - this.y_Coord));
+        }
+    }
+
+
+
---
+++ C_Sharp-OOP/03_CalcDistancePoint/03_PrintResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PrintResult
+{
+    static void Main()
+    {
+        Console.Write("Enter coordinates point p1: x = ");
+        int x_CoordP1 = Console.Read();
+        Console.ReadLine();
+        Console.Write("  y = ");
+        int y_CoordP1 = Console.Read();
+        Console.ReadLine();
+        Point p1 = new Point(x_CoordP1, y_CoordP1);
+
+        Console.Write("Enter coordinates point p2: x = ");
+        int x_CoordP2 = Console.Read();
+        Console.ReadLine();
+        Console.Write("  y = ");
+        int y_CoordP2 = Console.Read();
+        Console.ReadLine();
+        Point p2 = new Point(x_CoordP2, y_CoordP2);
+
+
+        Console.WriteLine("Distance p1 -> p2 = {0:f3}", p1.CalcDistance(p2));
+    }
+}
