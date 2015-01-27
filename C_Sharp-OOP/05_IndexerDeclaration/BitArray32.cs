820389a0a7769a0559bed493c92b30c8a1c79750
Magdalena Petrova <magik70@abv.bg>
2015-01-27 16:34:31 +0200

    05_IndexerDeclaration

---
+++ C_Sharp-OOP/05_IndexerDeclaration/BitArray32.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_IndexerDeclaration
+{
+    public class BitArray32
+    {
+        private int value;
+        //private static int bitCounter; //static member
+
+        public BitArray32(int value)
+        {
+            //bitCounter++; //static member does not use 'this'
+            this.value = value;
+        }
+
+        //indexer declaration
+        public int this[int index]
+        {
+            get
+            {
+                if(index < 0 || index > 31){
+                    throw new IndexOutOfRangeException("Index must be in the range[0...31].");
+                }
+                int bit = (this.value >> index) & 1;
+                return bit;
+            }
+            set
+            {
+                if (index < 0 || index > 31)
+                {
+                    throw new IndexOutOfRangeException("Index must be in the range[0...31].");
+                }
+                if (value != 0 && value != 1)
+                {
+                    throw new ArgumentException("Argument must be either 0 or 1.");
+                }
+                int mask = 1;
+                this.value &= ~(mask << index);//returned all bits in 0
+                this.value |= value << index;
+            }
+        }
+        //overloading operator '+', '+='
+        public static BitArray32 operator +(BitArray32 first, BitArray32 second)
+        {
+            int sum = first.value + second.value;
+            return new BitArray32(sum);
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();//print value to string
+        }
+    }
+}
---
+++ C_Sharp-OOP/05_IndexerDeclaration/BitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_IndexerDeclaration
+{
+    class BitTest
+    {
+        public static void Main()
+        {
+            int num = 7;
+            BitArray32 number = new BitArray32(num);
+            Console.Write("Input number: " + number);
+            Console.WriteLine("; bit at index 0: " + number[0] + ";"); //use index[0] -> bit = 1;
+            BitArray32 savedNumber = new BitArray32(num);
+            if (number[0] == 1)
+            {
+                number[0] = 0; //change bit
+            }
+            else if(number[0] == 0)
+            {
+                number[0] = 1; //change bit
+            }
+            Console.WriteLine("if bit = 1,make it equal 0,if bit = 0,make it 1;\r\nThe new number is: " + number); //print new number
+
+            BitArray32 sum = number + savedNumber;
+            Console.WriteLine(number + " + " + savedNumber + " = " + sum);
+            sum += sum;
+            Console.WriteLine("Sum += sum = {0}", sum);
+
+        }
+    }
+}
