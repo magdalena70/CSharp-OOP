e78f8efd63eb262996b3411e7a5b8aa66774be5e
Magdalena Petrova <magik70@abv.bg>
2015-01-27 16:35:53 +0200

    04_StaticClass

---
+++ C_Sharp-OOP/04_StaticClass/EmailValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace _04_StaticClass
+{
+    using System;
+    public static class EmailValidator
+    {
+        public static bool ValidateEmail(string email)
+        {
+            return email.Contains("@");
+        }
+    }
+}
---
+++ C_Sharp-OOP/04_StaticClass/SqrtPrecalculated.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_StaticClass
+{
+    public static class SqrtPrecalculated
+    {
+        //static fields(const is static default)
+        public const int MAX_VALUE = 10000;
+        private static int[] sqrtValues;
+
+        //static constructor
+        static SqrtPrecalculated()
+        {
+            sqrtValues = new int[MAX_VALUE + 1];
+            for (int i = 0; i < sqrtValues.Length; i++)
+            {
+                sqrtValues[i] = (int)Math.Sqrt(i);
+            }
+        }
+
+        //static methods
+        public static int ReturnSqrtValue(int value)
+        {
+            return sqrtValues[value];
+        }
+
+        static void Main() // is always static
+        {
+            Console.WriteLine("Sqrt = " + SqrtPrecalculated.ReturnSqrtValue(316));
+
+            string email = "absdfg@abv.bg";
+            bool isValid = EmailValidator.ValidateEmail(email);
+            Console.WriteLine("Is valid email: " + isValid);
+        }
+    }
+}
