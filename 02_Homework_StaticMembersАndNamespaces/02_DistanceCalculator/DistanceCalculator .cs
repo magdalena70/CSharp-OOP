fdd38ef3b94e9d780a97b0f4509ee7cad4db093c
Magdalena Petrova <magik70@abv.bg>
2015-01-28 22:25:52 +0200

    02_DistanceCalculator

---
+++ 02_Homework_StaticMembersАndNamespaces/02_DistanceCalculator/DistanceCalculator .cs
@@ -0,0 +1,22 @@
+﻿using _01_Point3D;
+using System;
+
+
+namespace _02_DistanceCalculator
+{
+    public static class DistanceCalculator 
+    {
+        public static double CalcDistance(Point3D pointOne, Point3D pointTwo)
+        {
+            //P1 = (x1, y1, z1)
+            //P2 = (x2, y2, z2)
+            //distance is SQRT((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
+
+            double deltaX = Math.Pow((pointOne.X - pointTwo.X), 2);//compute the squares of the first parameters to Math.Pow
+            double deltaY = Math.Pow((pointOne.Y - pointTwo.Y), 2);
+            double deltaZ = Math.Pow((pointOne.Z - pointTwo.Z), 2);
+
+            return Math.Sqrt(deltaX + deltaY + deltaZ);
+        }  
+    }
+}
---
+++ 02_Homework_StaticMembersАndNamespaces/02_DistanceCalculator/TestDistanceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using _01_Point3D;
+using System;
+
+namespace _02_DistanceCalculator
+{
+    public class TestDistanceCalculator
+    {
+        public static void Main()
+        {
+            Point3D p1 = new Point3D(1.0, 3.5, 8.2);
+            Point3D p2 = new Point3D(2.2, -7.0, -1.8);
+            Console.WriteLine("Distance between P1 and P2 in the 3D Euclidian space = {0:f3}.",
+                DistanceCalculator.CalcDistance(p1, p2));
+        }
+    }
+}
