9a743082eeffd9f744fcd3753bbc266261bdb04d
Magdalena Petrova <magik70@abv.bg>
2015-02-09 18:45:07 +0200

    08_Enum_Example

--- C_Sharp-OOP/08_Enum_Example/CofeeSize.cs
+++ C_Sharp-OOP/08_Enum_Example/CofeeSize.cs
@@ -4,7 +4,7 @@ namespace _08_Enum_Example
 {
     public enum CofeeSize
     {
-        Small = 100,
+        Small = 100, // can be number type int only
         Normal = 150,
         Double = 300
     }
