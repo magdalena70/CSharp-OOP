c7953005a70145eb0b3311ed6216a65877488c3b
Magdalena Petrova <magik70@abv.bg>
2015-02-10 16:40:56 +0200

    10_Generic_Example

---
+++ C_Sharp-OOP/10_Generic_Example/GenericList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _10_Generic_Example
+{
+    public class GenericList<T> where T : IComparable<T>
+    {
+        const int DefaultCapacity = 15;
+
+        private T[] elements;
+        private int count = 0;
+
+        public GenericList(int capacity = DefaultCapacity)
+        {
+            elements = new T[capacity];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (count >= elements.Length)
+            {
+                throw new IndexOutOfRangeException(String.Format(
+                    "The list capasity of {0} was exceeded.", elements.Length));
+                
+            }
+
+            this.elements[count] = element;
+            count++;
+        }
+
+        // indexator
+        public T this[int index]
+        {
+            get
+            {
+                // if elements[index] is empty -> throw Exeption
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Invalid index: {0}.", index));
+                }
+
+                T result = elements[index];
+                return result;
+            }
+            set
+            {
+                this.elements[index] = value;
+            }
+        }
+    }
+}
---
+++ C_Sharp-OOP/10_Generic_Example/TestGenericList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Generic_Example
+{
+    class TestGenericList
+    {
+        static void Main()
+        {
+            // declare list of type int
+            GenericList<int> intList = new GenericList<int>();
+            intList.Add(102);
+            intList.Add(2);
+            intList.Add(18);
+            intList.Add(4);
+            intList.Add(22);
+            intList.Add(309);
+            
+            int sum = 0;
+            Console.WriteLine("intList contains {0} elements: ", intList.Count);
+            for (int i = 0; i < intList.Count; i++)
+            {
+                // intList[intList.Count - 1] += intList[i]; // using setter of indexator
+                sum += intList[i];
+                Console.WriteLine(" {0}, ", intList[i]);
+            }
+
+            Console.WriteLine("Sum of numbers = {0}\n", sum);
+
+            // declare variable of type int and compare numbers
+            int intVariableToCompare = 10;
+            for (int i = 0; i < intList.Count; i++)
+            {
+                int minValue = Min(intList[i], intVariableToCompare);
+                Console.WriteLine("number {0}.Compare to number {1} => min value is {2}",
+                    intList[i], intVariableToCompare, minValue);
+            }
+
+            // declare list of type string
+            GenericList<string> strList = new GenericList<string>();
+            strList.Add("One");
+            strList.Add("Two");
+            strList.Add("Three");
+            strList.Add("the End");
+
+            Console.WriteLine("\nstrList contains {0} elements:", strList.Count);
+            for (int i = 0; i < strList.Count; i++)
+            {
+                Console.WriteLine(strList[i]);
+            }
+
+            Console.WriteLine();
+            // declare variable of type string and compare strings
+            string strVariableToCompare = "Two";
+            for (int i = 0; i < strList.Count; i++)
+            {
+                string minValueStr = Min(strList[i], strVariableToCompare);
+                Console.WriteLine("string '{0}'.Compare to string '{1}' => min value is '{2}'",
+                    strList[i], strVariableToCompare, minValueStr);
+            }
+        }
+
+        // generic method
+        public static T Min<T>(T first, T second)
+            where T : IComparable<T>
+        {
+            if (first.CompareTo(second) <= 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
