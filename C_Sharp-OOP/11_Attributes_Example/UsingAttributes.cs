1cb5dacf3bb9558fbb1dbd80c5cb504670c13951
Magdalena Petrova <magik70@abv.bg>
2015-02-10 17:18:22 +0200

    11_Attributes_Example

---
+++ C_Sharp-OOP/11_Attributes_Example/UsingAttributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _11_Attributes_Example
+{
+    class UsingAttributes
+    {
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+
+        public static extern int ShowMessageBox(int hWnd, string text, string caption, int type);
+
+        static void Main(string[] args)
+        {
+            ShowMessageBox(0, "some text", "some caption", 0);
+        }
+    }
+}
