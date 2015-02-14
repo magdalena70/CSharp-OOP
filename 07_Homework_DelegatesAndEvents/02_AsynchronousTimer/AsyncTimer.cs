f9fe7dd1400947f56fcc8bb05ccd4f3827a03ece
Magdalena Petrova <magik70@abv.bg>
2015-02-14 13:00:21 +0200

    02_AsynchronousTimer

---
+++ 07_Homework_DelegatesAndEvents/02_AsynchronousTimer/AsyncTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_AsynchronousTimer
+{
+    // Create a class AsyncTimer that executes a given method each t seconds.
+    class AsyncTimer
+    {
+        // The constructor should accept 3 arguments: 
+        // Action (a method to be called on every t milliseconds), 
+        // ticks (the number of times the method should be called) 
+        // and t (time interval between each tick in milliseconds).
+        private Action<string> actionMethod;
+        private int ticks;
+        private int timeInterval;
+
+        public AsyncTimer(Action<string> actionMethod, int ticks, int timeInterval)
+        {
+            this.actionMethod = actionMethod;
+            this.ticks = ticks;
+            this.timeInterval = timeInterval;
+        }
+
+        // The main program's execution should NOT be paused at any time (use some kind of background execution).
+        private void WorkingNonStop()
+        {
+            while (this.ticks > 0)
+            {
+                Thread.Sleep(this.timeInterval);
+
+                if (actionMethod != null)
+                {
+                    actionMethod(this.ticks + "");
+                }
+
+                this.ticks--;
+            }
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(this.WorkingNonStop);
+            thread.Start();
+        }
+    }
+}
---
+++ 07_Homework_DelegatesAndEvents/02_AsynchronousTimer/TestTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_AsynchronousTimer
+{
+    class TestTimer
+    {
+        public static void SaidGoodMorning(string str)
+        {
+            Console.WriteLine("Good Morning...");
+        }
+
+        public static void Work2(string str)
+        {
+            Console.Beep();
+        }
+
+
+
+        static void Main()
+        {
+             AsyncTimer timer = new AsyncTimer(SaidGoodMorning, 1000, 10);
+            timer.Start();
+
+            timer = new AsyncTimer(Work2, 500, 20);
+            timer.Start();
+        }
+    }
+}
