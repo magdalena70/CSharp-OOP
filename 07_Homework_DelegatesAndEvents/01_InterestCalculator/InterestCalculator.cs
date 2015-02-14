94b3adc0fab6c2b7324c5144df8f629380c6797b
Magdalena Petrova <magik70@abv.bg>
2015-02-14 12:32:40 +0200

    01_InterestCalculator

---
+++ 07_Homework_DelegatesAndEvents/01_InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+
+namespace _01_InterestCalculator
+{
+    // Create a delegate CalculateInterest (or use Func<…>) with parameters 
+    // sum of money, interest and years that calculates the interest according to the method it points to. 
+    public delegate decimal CalculateInterest(decimal sumOfMoney, decimal interest, int years);
+
+    // Create a class InterestCalculator that takes in its constructor 
+    // money, interest, years and interest calculation delegate. 
+    class InterestCalculator
+    {
+        private decimal money;
+        private decimal interest;
+        private int years;
+        private CalculateInterest calcInterestDelegate;
+
+        public InterestCalculator(decimal money, decimal interest, int years, CalculateInterest calcInterestDelegate)
+        {
+            this.Money = money;
+            this.Interest = interest;
+            this.Years = years;
+            this.CalcInterestDelegate = calcInterestDelegate;
+        }
+
+        public decimal Money 
+        { 
+            get
+            {
+                return this.money; 
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative.");
+                }
+                this.money = value;
+            } 
+        }
+
+        public decimal Interest 
+        {
+            get 
+            {
+                return this.interest;
+            }
+            private set
+            {
+                this.interest = value;
+            } 
+        
+        }
+
+        public int Years
+        {
+            get 
+            {
+                return this.years;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Years cannot be negative.");
+                }
+
+                this.years = value;
+            } 
+        }
+
+        public CalculateInterest CalcInterestDelegate 
+        {
+            get
+            {
+                return this.calcInterestDelegate;
+            }
+            private set
+            {
+                this.calcInterestDelegate = value;
+            } 
+        
+        }
+
+        // The result should be rounded to 4 places after the decimal point.
+        public override string ToString()
+        {
+            return string.Format("{0:F4}", this.CalcInterestDelegate(this.Money, this.Interest, this.Years));
+        }
+    }
+}
---
+++ 07_Homework_DelegatesAndEvents/01_InterestCalculator/TestInterestCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01_InterestCalculator
+{
+    class TestInterestCalculator
+    {
+        // Create a method GetSimpleInterest(sum, interest, years). 
+        // The interest should be calculated by the formula A = sum * (1 + interest * years).
+        public static decimal GetSimpleInterest(decimal sum, decimal interest, int years)
+        {
+            decimal simpleInterest = sum * (1 + (interest / 100) * years);
+            return simpleInterest;
+        }
+
+        // Create a method GetCompoundInterest(sum, interest, years).  
+        // The interest should be calculated by the formula A = sum * (1 + interest / n)year * n, 
+        // where n is the number of times per year the interest is compounded. Assume n is always 12.
+        public static decimal GetCompoundInterest(decimal sum, decimal interest, int years)
+        {
+            decimal compoundInterest = sum * (decimal)Math.Pow((double)(1 + (interest / 100) / 12), years * 12);
+            return compoundInterest;
+        }
+
+        public static void Main()
+        {
+            InterestCalculator simpleInterestCalc = new InterestCalculator(2500m, 7.2m, 15, GetSimpleInterest);
+            Console.WriteLine(simpleInterestCalc);
+
+            InterestCalculator compoundInterestCalc = new InterestCalculator(-500m, 5.6m, 10, GetCompoundInterest);
+            Console.WriteLine(compoundInterestCalc);
+
+            /*CalculateInterest calcInterest = new CalculateInterest(GetSimpleInterest);
+            // The result should be rounded to 4 places after the decimal point.
+            Console.WriteLine("{0:F4}", calcInterest(2500m, 7.2m, 15));
+            calcInterest = GetCompoundInterest;
+            Console.WriteLine("{0:F4}", calcInterest(500m, 5.6m, 10));*/
+        }
+    }
+}
