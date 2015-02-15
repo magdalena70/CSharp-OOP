a662df8a26ede1eb66f728f898798aacf0556518
Magdalena Petrova <magik70@abv.bg>
2015-02-15 18:40:20 +0200

    01_StringBuilderExensions

---
+++ 08_HomeworkFunctionalProgramming/01_StringBuilderExtensions/StringBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_StringBuilderExtensions
+{
+    // create static class for extension methods
+    public static class StringBuilderExtensions
+    {
+
+        // Implement the following extension methods for the class StringBuilder:
+
+        // Substring(int startIndex, int length) – 
+        // returns a new String object, containing the elements in the given range. 
+        // Throw an exception when the range is invalid.
+        public static string Substring(this StringBuilder strBuilder, int startIndex, int length)
+        {
+            string strAsString = strBuilder.ToString();
+            string substring = strAsString.Substring(startIndex, length);
+            return substring;
+        }
+
+        // RemoveText(string text) – 
+        // removes all occurrences of the specified text (case-insensitive) from the StringBuilder. 
+        // The method should not create a new StringBuilder, but should modify the existing one and return it as a result.
+        public static StringBuilder RemoveText(this StringBuilder strBuilder, string text)
+        {
+            var strAsString = strBuilder.ToString();
+            while (true)
+            {
+                var textPosition = strAsString.IndexOf(text, StringComparison.CurrentCultureIgnoreCase);
+                if (textPosition < 0)
+                {
+                    break;
+                }
+
+                var textLenght = text.Length;
+                strAsString = strAsString.Remove(textPosition, textLenght);
+            }
+
+            return strBuilder =  new StringBuilder(strAsString);
+        }
+
+        // AppendAll<T>(IEnumerable<T> items) – 
+        // appends the string representations of all items from the specified collection. 
+        // Use ToString() to convert from T to string.
+        public static StringBuilder AppendAll<T>(this StringBuilder strBuilder, IEnumerable<T> items)
+        {
+            var resultString = items.Aggregate("", (current, item) => current + item.ToString());
+            return strBuilder.Append(resultString);
+        }
+    }
+}
---
+++ 08_HomeworkFunctionalProgramming/01_StringBuilderExtensions/TestExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_StringBuilderExtensions
+{
+    class TestExtensionMethods
+    {
+        static void Main()
+        {
+            var test = new StringBuilder("StringBuilder Extensions");
+            Console.WriteLine("string -> \"{0}\"", test);
+            Console.WriteLine("test.Substring(3, 9) -> \"{0}\"", test.Substring(3, 9));
+            Console.WriteLine("test.RemoveText(\"Exten\") -> \"{0}\"", test.RemoveText("Exten"));
+            // test StringComparison.CurrentCultureIgnoreCase
+            Console.WriteLine("test.RemoveText(\"stringbuilder\") -> \"{0}\"", test.RemoveText("stringbuilder"));
+            Console.Write("test.AppendAll(string) -> \"");
+            Console.WriteLine(test.AppendAll(
+                new string[] {
+                    ":\n",
+                    " Substring(int startIndex, int length),\n",
+                    " RemoveText(string text),\n",
+                    " AppendAll<T>(IEnumerable<T> items).\"" }));
+        }
+    }
+}
