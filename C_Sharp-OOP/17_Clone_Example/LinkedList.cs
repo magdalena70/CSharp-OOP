9001505db8240fe4d57b459c8fbd3aa3811fc224
Magdalena Petrova <magik70@abv.bg>
2015-02-19 13:12:49 +0200

    17_Clone_Example

---
+++ C_Sharp-OOP/17_Clone_Example/LinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace _17_Clone_Example
+{
+    public class LinkedList<T> : ICloneable
+    {
+        private T tValue;
+        private LinkedList<T> nextNode;
+
+        public LinkedList(T tValue, LinkedList<T> nextNode = null)
+        {
+            this.TValue = tValue;
+            this.NextNode = nextNode;
+        }
+
+        public T TValue
+        {
+            get{return this.tValue;}
+            set{this.tValue = value;}
+        }
+
+        public LinkedList<T> NextNode
+        {
+            get { return this.nextNode; }
+            set { this.nextNode = value; }
+        }
+
+        public LinkedList<T> Clone() // deep clonning
+        {
+            // copy the first element
+            LinkedList<T> original = this;
+            T valueOriginal = original.TValue;
+            LinkedList<T> result = new LinkedList<T>(TValue); // new object (new instance)
+            LinkedList<T> copy = result;
+            original = original.NextNode;
+
+            // copy the rest of the elements
+            while (original != null)
+            {
+                valueOriginal = original.TValue;
+                copy.NextNode = new LinkedList<T>(valueOriginal);
+                original = original.NextNode;
+                copy = copy.NextNode;
+            }
+
+            return result;
+        }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        public LinkedList<T> ShallowCopy() // shallow clonning
+        {
+            return this; // the same object
+        }
+
+        public LinkedList<T> MemberwiseCopy()
+        {
+            return (LinkedList<T>)this.MemberwiseClone(); // shallow copy for each memeber
+        }
+
+        public override string ToString()
+        {
+            LinkedList<T> currentNode = this;
+            StringBuilder sb = new StringBuilder("( ");
+            while (currentNode != null)
+            {
+                sb.Append(currentNode.TValue);
+                currentNode = currentNode.NextNode;
+                    if (currentNode != null)
+                    {
+                    sb.Append(", ");
+                    }
+            }
+            sb.Append(" )");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C_Sharp-OOP/17_Clone_Example/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Clone_Example
+{
+    class Test
+    {
+        public static void Main()
+        {
+            LinkedList<string> initialList = // recursive
+                new LinkedList<string>("John",
+                    new LinkedList<string>("Jaga",
+                        new LinkedList<string>("Kiro")));
+
+            LinkedList<string> deeplyClone = initialList.Clone();
+            deeplyClone.TValue = "1st value";
+            deeplyClone.NextNode.TValue = "2nd value";
+            deeplyClone.NextNode.NextNode.TValue = "3d value";
+
+            Console.WriteLine("Inicial list = {0}", initialList);
+            Console.WriteLine("Deeply clonning list = {0}", deeplyClone);
+        }
+    }
+}
