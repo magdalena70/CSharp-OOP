2548707676f4b46d9b4be62794774452cdcbf055
Magdalena Petrova <magik70@abv.bg>
2015-02-23 15:04:20 +0200

    19_Using_ref_Example

---
+++ C_Sharp-OOP/19_Using_ref_Example/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace _19_Using_ref_Example
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        // by reference
+        public static void ModifyPersonByRef(ref Person person)
+        {
+            person = new Person("Modified Name by ref", person.Age);
+        }
+
+        public static void ModifyPerson(Person person)
+        {
+            person = new Person("Modified Name without ref", person.Age);
+        }
+    }
+}
---
+++ C_Sharp-OOP/19_Using_ref_Example/TestPerson.cs
@@ -0,0 +1,23 @@
+﻿namespace _19_Using_ref_Example
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestPerson
+    {
+
+        public static void Main()
+        {
+            Person person = new Person("Dimitar Zahariev", 22);
+            Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+            Person.ModifyPerson(person); // not modified
+            Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+            Person.ModifyPersonByRef(ref person); // modified
+            Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+        }
+    }
+}
