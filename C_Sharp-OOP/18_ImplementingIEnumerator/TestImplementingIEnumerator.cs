11daefccf521b98824d9ee7ab4987e2cd787172b
Magdalena Petrova <magik70@abv.bg>
2015-02-23 11:15:19 +0200

    18_ImplementingIEnumerator

--- C_Sharp-OOP/18_ImplementingIEnumerator/TestImplementingIEnumerator.cs
+++ C_Sharp-OOP/18_ImplementingIEnumerator/TestImplementingIEnumerator.cs
@@ -14,8 +14,24 @@
                         new LinkedList<string> ("Third element")));
             foreach (var element in ienumerableLinkedList)
             {
-                Console.WriteLine(element);
+                Nullable<decimal> number = null;
+                Console.WriteLine("LinkedList -> {0},\nNullable -> {1}", 
+                    element, number.GetValueOrDefault());
             }
+
+            Console.WriteLine();
+            ValueType valType = 10;
+            Console.WriteLine("ValueType valType = {0}, {0} is {1}",valType, valType.GetType());
+            valType = 6.3;
+            Console.WriteLine("valType = {0}, {0} is {1}", valType, valType.GetType());
+            valType = 10000000000000000000;
+            Console.WriteLine("valType = {0}, {0} is {1}", valType, valType.GetType());
+            decimal numDec = 100;
+            valType = numDec; // boxing (stak -> heap)
+            Console.WriteLine("decimal numDec = {0}, valType = numDec, valType is {1}", numDec, valType.GetType());
+            //int numInt = (int)valType; // return InvalidCastException
+            int numInt = (int)(decimal)valType; // unboxing (heap -> stak)
+            Console.WriteLine("int numInt = (int)(decimal)valType, numInt = {0}", numInt);
         }
     }
 }
