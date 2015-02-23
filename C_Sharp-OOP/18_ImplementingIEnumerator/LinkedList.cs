9eb8e7e7dd244475a6d85033447b6d6122026afe
Magdalena Petrova <magik70@abv.bg>
2015-02-23 09:42:41 +0200

    18_ImplementingIEnumerator

---
+++ C_Sharp-OOP/18_ImplementingIEnumerator/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿namespace _18_ImplementingIEnumerator
+{
+
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public T Value { get; set; }
+        public LinkedList<T> NextNode { get; private set; }
+
+        public LinkedList(T value, LinkedList<T> nextNode = null)
+        {
+            this.Value = value;
+            this.NextNode = nextNode;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator(); // Call the generic version of the method
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedList<T> currentNode = this;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value; // remember current value
+                currentNode = currentNode.NextNode;
+            }
+        }
+    }
+}
---
+++ C_Sharp-OOP/18_ImplementingIEnumerator/TestImplementingIEnumerator.cs
@@ -0,0 +1,21 @@
+﻿namespace _18_ImplementingIEnumerator
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class TestImplementingIEnumerator
+    {
+        public static void Main()
+        {
+            LinkedList<string> ienumerableLinkedList = 
+                new LinkedList<string> ("First element",
+                    new LinkedList<string> ("Second element",
+                        new LinkedList<string> ("Third element")));
+            foreach (var element in ienumerableLinkedList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
